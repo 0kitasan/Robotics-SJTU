c0bf963d2c09029e37d2db5f4436549a65687a27
0kitasan <292823441@qq.com>
2025-10-29 20:05:24 +0800

    renew README

--- README.md
+++ README.md
@@ -2,3 +2,23 @@
 
 AU3307
 
+## 机械臂仿真操作
+
+`Dofbot_2025/`
+
+### RobotToolbox Python
+
+`dh_kine_student.py`
+
+### 仿真环境(PyBullet)采集数据
+
+dofbot 机械臂末端位姿与关节角
+
+### 机械臂正逆运动学模型学习
+
+`dofbot_learn_student.py`
+
+## 实机实验
+
+`dofbot_ctrl/`
+
--- script.sh
+++ script.sh
@@ -1,10 +1,9 @@
+# docker 中的 ros 需要 source
 source /opt/ros/noetic/setup.bash
 
-# export本机ip地址
+# export 本机 ip 地址
 
 export ROS_HOSTNAME=192.168.31.51
 
 export ROS_MASTER_URI=http://192.168.31.161:11311
 
-
-ROS_MASTER_URI=http://192.168.1.123:11311
