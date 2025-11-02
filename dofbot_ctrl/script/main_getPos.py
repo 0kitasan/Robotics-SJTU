fc7f7490234894373099d785191e84c60e7bcbc7
0kitasan <292823441@qq.com>
2025-11-02 10:42:41 +0800

    renew

--- dofbot_ctrl/script/main_getPos.py
+++ dofbot_ctrl/script/main_getPos.py
@@ -11,6 +11,8 @@ def linear_interpolation(src, tat, n=10):
     return path
 
 # 2 号机器 关闭 gripper 150  开启 gripper 90
+GRIPPER_OPEN=90
+GRIPPER_CLOSE=135
 
 if __name__ == '__main__':
     # 调用realenv
@@ -28,46 +30,42 @@ if __name__ == '__main__':
     # 状态机的中间路点
     points = [
         np.asarray([90., 90., 90., 90., 90.]), # 初始位置
-        np.asarray([137., 51., 52., 2., 90.]), # 第一个目标位置 修改
-        np.asarray([40., 61., 42., 7., 90.]), # 第二个目标位置 修改
-    ] 
+        np.asarray([137., 48., 52., 2., 90.]), # 第一个目标位置 修改
+        np.asarray([137., 56., 52., 2., 90.]),
+        np.asarray([40., 50., 45., 7., 90.]), # 第二个目标位置 修改
+    ]
 
-    # 2 号机器 关闭 gripper 150  开启 gripper 90
-
+    # 2 号机器 关闭 gripper 135  开启 gripper 90
+    env.step(gripper=GRIPPER_OPEN)
     for i in range(len(points) - 1):
         # 取出路点并做路径规划得到路径
-        path = linear_interpolation(points[i], points[i + 1], n=10)
-        
+        path = linear_interpolation(points[i], points[i + 1], n=15)
+
         print(f"current i: {i}")
+        print(f"{path}")
 
         for j,p in enumerate(path):
             # 建议分开控制
-            print(f"{path}")
-
             length = len(path)
 
             print(f"length: {length}, current j: {j}")
 
             if i == 0:
                 if j == length - 1: #  假设在第一个路径段打开夹爪，再关闭
-                    env.step(joint=p, gripper=90) 
-                    time.sleep(0.1)
-                    env.step(joint=p, gripper=150)
                     print("进入抓取最后一段")
-
-                else:
-                    env.step(joint=p, gripper=90) 
-
-                
-
-
-            if i == 1:
-                if j == length - 1:
-                    env.step(joint=p, gripper=90) 
+                    env.step(joint=p, gripper=GRIPPER_OPEN) 
                     time.sleep(0.1)
-                    # env.step(joint=p, gripper=150)
-                    print("进入放置最后一段")
+                    env.step(joint=p, gripper=GRIPPER_CLOSE)
                 else:
-                    env.step(joint=p, gripper=150) 
+                    env.step(joint=p, gripper=GRIPPER_OPEN) # 123
+
+
+            if i == 2:
+                if j == length - 1:
+                    print("进入放置最后一段")
+                    env.step(joint=p, gripper=GRIPPER_OPEN) 
+                    time.sleep(0.1)
+                else:
+                    env.step(joint=p, gripper=GRIPPER_CLOSE) 
 
 
